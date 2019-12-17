5ae0857604a3263281d475c146fa28549c228366
DESKTOP-DEL3KK9\DungNguyen <nguyendung300694@gmail.com>
2019-12-18 01:08:10 +0700

    ewdsf

--- TangTinhCac/Scripts/src/index.js
+++ TangTinhCac/Scripts/src/index.js
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './pages/Home'
 
-const App = () => <div>Hello world!</div>;
+const App = () => <Home/>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ TangTinhCac/Scripts/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Hello, I'm Testing ReactJS</h1>
+      </div>
+    )
+  }
+}
