e5b31ac7d2445d83508a22ae47374152a3b7adb0
Le Cong Dat <lecongdatmin@outlook.com.vn>
2019-12-23 23:06:20 +0700

    edit Idol Forum commit 23/12/19 11:01pm

--- TangTinhCac/Scripts/src/components/IdolForum.js
+++ TangTinhCac/Scripts/src/components/IdolForum.js
@@ -1,13 +1,1 @@
-﻿import React, { Component } from 'react';
-
-export default class IdolForum extends Component {
-    render() {
-        return (
-            <div id="idol_forum" className="hero-wrap js-fullheight" data-stellar-background-ratio="0.5">
-                <div className="js-fullheight d-flex justify-content-center p-5">
-
-                </div>
-            </div>
-        );
-    }
-}
+﻿
